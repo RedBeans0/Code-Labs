9c0f7ec94eeca763d46a28f3db63f5aef8be8d95
RedBeans0 <Thunderkracker934@gmail.com>
2022-10-04 13:21:56 -0500

    Exam

---
+++ Exam-1/data-types/jav.js
@@ -0,0 +1,19 @@
+num = 1
+
+beans = "BEANS"
+
+comma = ","
+
+let x = NaN;
+
+const Names = [ "chris", "clark", "Joe"]
+
+const ages = { 
+
+    One: "21",
+    Two: "18",
+    Three: "38"
+
+};
+
+console.log( num + " can of " + beans + " can make " + Names[2] + comma +" a man who's " + ages.Three + comma + "feel more full than they were before." + "but we all know that the true way to be more full is with [" + x + "]");
---
+++ Exam-1/dom/dom.js
@@ -0,0 +1,12 @@
+var post = ['title', 'description'];
+title = "The Anchient Civilization of Rome."
+desc = "In modern historgraphy, anchient Rome refers to roman civilization from the founding of the city of Rome in the 8th century BC to the collapse of the western Roman Empire in the 5th century AD. It encompasses the Roman Kingdom, Roman Empire until the fall of the western Empire."
+
+
+function head() {
+    document.getElementById("title").innerHTML = title;
+}
+
+function stuff() {
+
+}
