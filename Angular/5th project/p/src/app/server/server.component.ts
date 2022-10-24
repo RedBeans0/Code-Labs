808bc09380598aa9875363a6abcdb13908faf033
RedBeans0 <Thunderkracker934@gmail.com>
2022-10-24 14:23:05 -0500

    aaaaaaaa

--- Angular/5th project/p/src/app/server/server.component.ts
+++ Angular/5th project/p/src/app/server/server.component.ts
@@ -4,11 +4,19 @@ import { Component, OnInit } from '@angular/core';
   selector: 'app-server',
   templateUrl: './server.component.html'
 })
-export class ServerComponent implements OnInit {
+export class ServerComponent {
+seerverId: number = 10;
+serverStatus: string = 'offline';
+  constructor() {
+    this.serverStatus = Math.random() > 0.5 ? 'online' : 'offline';
+   }
 
-  constructor() { }
+   getServerStatus() {
+    return this.serverStatus;
+   }
 
-  ngOnInit(): void {
-  }
+   getColor() {
+    return this.serverStatus === 'online' ? 'green' : 'red'
+   }
 
 }
